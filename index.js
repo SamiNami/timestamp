76adfb8d2ce415cc67f0d2110fc040a760a18945
sami peralahti <sami.peralahti@vainu.io>
2018-07-12 20:54:16 -0700

    improved the logic

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ const app = express();
 // if not timestamp is provided use the time of right now
 app.get('/api/timestamp/', (req, res) => {
     const date = new Date();
+
     res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
 app.get('/api/timestamp/:date_string', (req, res) => {
-    let { date_string } = req.params;
-    date_string = new Date(parseInt(date_string));
+    const { date_string } = req.params;
+    let date = Number(date_string) || date_string;
+    date = new Date(date);
 
-    if (!Date.parse(date_string)) {
+    if (isNaN(date)) {
         res.json({ error: 'Invalid Date' });
+        return;
     }
-    const date = new Date(date_string);
-
     res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
