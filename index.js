4870559e4466453b19fa37e46b115ac7381ab32d
sami peralahti <sami.peralahti@vainu.io>
2018-07-12 17:20:43 -0700

    app works, microservice for parsing dates

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hello: 'world' });
+// if not timestamp is provided use the time of right now
+app.get('/api/timestamp/', (req, res) => {
+    const date = new Date();
+
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
+
+app.get('/api/timestamp/:time', (req, res) => {
+    if (!Date.parse(req.params.time)) {
+        res.json({ error: 'Invalid Date' });
+    }
+    const date = new Date(req.params.time);
+
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
 const PORT = process.env.PORT || 3000;
