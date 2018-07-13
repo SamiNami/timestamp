135a66952d227a23ed4e7074059cc1ac242273d9
sami peralahti <sami.peralahti@vainu.io>
2018-07-12 20:10:32 -0700

    added suport for unix timestamps

--- index.js
+++ index.js
@@ -5,15 +5,17 @@ const app = express();
 // if not timestamp is provided use the time of right now
 app.get('/api/timestamp/', (req, res) => {
     const date = new Date();
-
     res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
-app.get('/api/timestamp/:time', (req, res) => {
-    if (!Date.parse(req.params.time)) {
+app.get('/api/timestamp/:date_string', (req, res) => {
+    let { date_string } = req.params;
+    date_string = new Date(parseInt(date_string));
+
+    if (!Date.parse(date_string)) {
         res.json({ error: 'Invalid Date' });
     }
-    const date = new Date(req.params.time);
+    const date = new Date(date_string);
 
     res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
